8338fbc1f62c3d6d24640c218fde55d5e0ee2dae
10Best Living <dev@10bestliving.com>
2026-09-08 14:38:23 +0800

    Add website logo, fix dist tracking, improve favicon and header branding

--- add_favicon.cjs
+++
@@ -1,6 +0,0 @@
-const fs = require('fs');
-let c = fs.readFileSync('src/layouts/BaseLayout.astro', 'utf8');
-const newTitle = '<title>{title} - LuxeRank</title>\n    <link rel="icon" type="image/svg+xml" href="/favicon.svg" />\n    <link rel="icon" type="image/png" sizes="32x32" href="/favicon.png" />';
-c = c.replace('<title>{title} - LuxeRank</title>', newTitle);
-fs.writeFileSync('src/layouts/BaseLayout.astro', c);
-console.log('Favicon links added to BaseLayout');
